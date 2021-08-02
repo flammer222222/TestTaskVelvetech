b7da4893abf0f514044b28cfdaacadc3b5ef3659
Alamez <ereminalesha222222@gamil.com>
2021-08-02 13:26:37 +0300

    настроены простые передача и прием данных через сокеты на порте 9090

--- server/server.py
+++ server/server.py
@@ -1,7 +1,23 @@
 from filter.filter import FilterBadWords
+import socket
 
+
+class Server:
+    def __init__(self):
+        sock = socket.socket()
+        sock.bind(('', 9090))
+        sock.listen(1)
+        self.conn, addr = sock.accept()
+
+    def recive(self):
+        while True:
+            data = self.conn.recv(1024)
+            if not data:
+                break
+            self.conn.send(data.upper())
+            print(data.decode('utf-8'))
+        self.conn.close()
 
 if __name__ == '__main__':
-    test_string = "rkrrk aslpsap anus slfd sfaf anal analsssd ddd bunny fucker"
-    my_filter = FilterBadWords('../filter/resources/list_of_bad_words.txt')
-    print(my_filter.filter(test_string))
+    my_server = Server()
+    my_server.recive()
